03de2966a1274828147b9c3a833f79795a1565af
tasha <tasha.wallage@gmail.com>
2021-04-30 00:20:04 -0700

    updating port to work with Heroku

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { Note } = require('./model/note');
 require('dotenv').config();
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'view/build')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -40,7 +40,7 @@ app.post('/api/notes', async (req, res) => {
 });
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'view/build', 'index.html'));
 });
 
 const PORT = process.env.PORT || 8000;
