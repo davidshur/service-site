6d594edf8e39a72fdae141ef776cff86988b789e
David <david@thejurne.com>
2021-04-30 01:19:03 -0700

    working website full site

--- view/src/components/ThoughtsAndPrayersForm.js
+++ view/src/components/ThoughtsAndPrayersForm.js
@@ -1,8 +1,21 @@
+import { useState } from 'react';
+import axios from 'axios';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Form from 'react-bootstrap/Form';
 
 function ThoughtsAndPrayersForm() {
+  const [name, setName] = useState('');
+  const [message, setMessage] = useState('');
+  const [confirmed, setConfirmed] = useState(false);
+
+  async function handleSubmit() {
+    await axios.post('/api/notes', { name, message });
+    setName('');
+    setMessage('');
+    setConfirmed(true);
+  }
+
   return (
     <div className="col-12 col-md-4">
       <h3>Share Thoughts and Prayers</h3>
@@ -10,18 +23,19 @@ function ThoughtsAndPrayersForm() {
       <Card className="mb-3">
         <Card.Body>
           <Form>
-            <Form.Group controlId="formName">
+            <Form.Group>
               <Form.Label>Name</Form.Label>
-              <Form.Control type="email" placeholder="Enter your name" />
+              <Form.Control type="text" value={name} onChange={(event) => setName(event.target.value)} placeholder="Enter your name" />
             </Form.Group>
-            <Form.Group controlId="exampleForm.ControlTextarea1">
+            <Form.Group>
               <Form.Label>Message</Form.Label>
-              <Form.Control as="textarea" placeholder="Enter your thoughts and prayers" rows={3} />
+              <Form.Control as="textarea" value={message} onChange={(event) => setMessage(event.target.value)} placeholder="Enter your thoughts and prayers" rows={3} />
               <Form.Text className="text-muted">
                 Note: Your name and message may be visible to other website visitors.
               </Form.Text>
             </Form.Group>
-            <Button variant="primary" type="submit">
+            {confirmed && <p style={{ fontSize: '12px' }} className="text-success">Your thought or prayer has been saved. You might see it on the thoughts and prayers page soon!</p>}
+            <Button variant="primary" onClick={handleSubmit}>
               Share
             </Button>
           </Form>
