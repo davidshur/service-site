ba07eaf836aa1c72f748031d26fa5a319729ba8d
David <david@thejurne.com>
2021-04-30 01:21:18 -0700

    update eulogy

--- view/src/components/Eulogy.js
+++ view/src/components/Eulogy.js
@@ -2,19 +2,19 @@ function Eulogy() {
     return (
         <div className="col-md-4 col-12">
             <h3>Eulogy</h3>
-            <p className="text-muted">By: Anonymous</p>
+            <p className="text-muted">By: Rumeet Huynh</p>
             <hr />
-            <p>I have the impossible task of summarizing John Huynhs life.</p>
-            <p>John was loved by everyone, I can say that confidently.</p>
-            <p>He was born in Lancaster , Pennsylvania in 1992 to Long and
+            <p>I have the impossible task of summarizing John Huynh's life.
+                John was loved by everyone, I can say that confidently.
+                </p><p>He was born in Lancaster , Pennsylvania in 1992 to Long and
                 Anh. He was the oldest of 3 children as the big brother to
                 Binh and Linda. He lived in Pennsylvania until 2014 when he
                 moved out to Washington. He then met his beloved wife, Rumeet,
                 in 2015. John worked in the health insurance industry as well
-                as being a local business owner. John enjoyed music, martial arts,
+                as being a local business owner. </p><p>John enjoyed music, martial arts,
                 and looking sharp for any occasion. His number one goal in life was
                 to honor his mother and build a successful way to provide for his family.
-                He was always focused on improving the kind of person he was and seeking
+                </p><p>He was always focused on improving the kind of person he was and seeking
                 the truth. This led him to finding and building a relationship with the Lord in 2019.
                 This change affected every aspect of his life going forward and John felt called to
                 live a life that exemplified Christ’s love. He is a loving son, brother, husband,
