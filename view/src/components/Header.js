b6af34e53824b64ac3665e178ca51645cb8fdad1
David <david@thejurne.com>
2021-04-29 22:24:01 -0700

    router

--- view/src/components/Header.js
+++ view/src/components/Header.js
@@ -1,5 +1,18 @@
+import { Link } from 'react-router-dom';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
 function Header() {
-  return <p>Header</p>;
+  return (
+    <Navbar bg="light" variant="light">
+      <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+      <Nav className="ml-auto">
+        <Nav.Link as={Link} href="#home">Home</Nav.Link>
+        <Nav.Link as={Link} href="#features">Features</Nav.Link>
+        <Nav.Link as={Link} href="#pricing">Pricing</Nav.Link>
+      </Nav>
+    </Navbar>
+  );
 }
 
 export default Header;
