a38918f5c4214ccb57f2c7bf69e0a7616500998a
tasha <tasha.wallage@gmail.com>
2021-04-30 00:13:59 -0700

    updating port to work with Heroku

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server is listening on port: ${PORT}!!`);
 });
