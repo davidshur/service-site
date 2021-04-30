3edeeadb9ade9f91815f613056cd84843971382c
tasha <tasha.wallage@gmail.com>
2021-04-29 21:50:37 -0700

    Adding images, eulogy, and cleaning up App.js

---
+++ view/src/components/Eulogy.js
@@ -0,0 +1,23 @@
+function Eulogy() {
+    return (
+        <div>
+            <p>I have the impossible task of summarizing John Huynhs life.</p>
+            <p>John was loved by everyone, I can say that confidently.</p>
+            <p>He was born in Lancaster , Pennsylvania in 1992 to Long and
+                Anh. He was the oldest of 3 children as the big brother to
+                Binh and Linda. He lived in Pennsylvania until 2014 when he
+                moved out to Washington. He then met his beloved wife, Rumeet,
+                in 2015. John worked in the health insurance industry as well
+                as being a local business owner. John enjoyed music, martial arts,
+                and looking sharp for any occasion. His number one goal in life was
+                to honor his mother and build a successful way to provide for his family.
+                He was always focused on improving the kind of person he was and seeking
+                the truth. This led him to finding and building a relationship with the Lord in 2019. T
+                his change affected every aspect of his life going forward and John felt called to
+                live a life that exemplified Christ’s love. He is a loving son, brother, husband,
+                and a true follower of Christ.</p>
+        </div>
+    )
+}
+
+export default Eulogy;
